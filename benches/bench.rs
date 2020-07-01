706b449b5467516cbcf940575ba9c6f8a3ac36e1
Deirdre Connolly <deirdre@zfnd.org>
2020-07-01 16:45:29 -0400

    Tidy benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,7 +4,7 @@ use rand::thread_rng;
 use redjubjub::*;
 use std::convert::TryFrom;
 
-fn sigs_with_distinct_pubkeys<T: SigType>(
+fn sigs_with_distinct_keys<T: SigType>(
 ) -> impl Iterator<Item = (VerificationKeyBytes<T>, Signature<T>)> {
     std::iter::repeat_with(|| {
         let sk = SigningKey::<T>::new(thread_rng());
@@ -14,21 +14,13 @@ fn sigs_with_distinct_pubkeys<T: SigType>(
     })
 }
 
-fn sigs_with_same_pubkey<T: SigType>(
-) -> impl Iterator<Item = (VerificationKeyBytes<T>, Signature<T>)> {
-    let sk = SigningKey::<T>::new(thread_rng());
-    let vk_bytes = VerificationKey::from(&sk).into();
-    std::iter::repeat_with(move || {
-        let sig = sk.sign(thread_rng(), b"");
-        (vk_bytes, sig)
-    })
-}
-
 fn bench_batch_verify(c: &mut Criterion) {
     let mut group = c.benchmark_group("Batch Verification");
     for n in [8usize, 16, 24, 32, 40, 48, 56, 64].iter() {
         group.throughput(Throughput::Elements(*n as u64));
-        let sigs = sigs_with_distinct_pubkeys().take(*n).collect::<Vec<_>>();
+
+        let sigs = sigs_with_distinct_keys().take(*n).collect::<Vec<_>>();
+
         group.bench_with_input(
             BenchmarkId::new("Unbatched verification", n),
             &sigs,
@@ -41,22 +33,9 @@ fn bench_batch_verify(c: &mut Criterion) {
                 })
             },
         );
+
         group.bench_with_input(
-            BenchmarkId::new("Signatures with Distinct Pubkeys", n),
-            &sigs,
-            |b, sigs| {
-                b.iter(|| {
-                    let mut batch = batch::Verifier::<SpendAuth>::new();
-                    for (vk_bytes, sig) in sigs.iter().cloned() {
-                        batch.queue((vk_bytes, sig, b""));
-                    }
-                    batch.verify(thread_rng())
-                })
-            },
-        );
-        let sigs = sigs_with_same_pubkey().take(*n).collect::<Vec<_>>();
-        group.bench_with_input(
-            BenchmarkId::new("Signatures with the Same Pubkey", n),
+            BenchmarkId::new("Batched verification", n),
             &sigs,
             |b, sigs| {
                 b.iter(|| {
