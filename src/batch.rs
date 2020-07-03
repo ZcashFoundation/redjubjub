c35946b18b73740b2bd8a85e92d9b6ba1e3e896d
Deirdre Connolly <deirdre@zfnd.org>
2020-07-02 22:23:13 -0400

    Make Verify agnostic to the SigType

--- src/batch.rs
+++ src/batch.rs
@@ -13,10 +13,7 @@ use std::convert::TryFrom;
 use jubjub::*;
 use rand_core::{CryptoRng, RngCore};
 
-use crate::{
-    scalar_mul::VartimeMultiscalarMul, Error, HStar, Scalar, SigType, Signature, VerificationKey,
-    VerificationKeyBytes,
-};
+use crate::{private::Sealed, scalar_mul::VartimeMultiscalarMul, *};
 
 // Shim to generate a random 128bit value in a [u64; 4], without
 // importing `rand`.
@@ -27,21 +24,68 @@ fn gen_128_bits<R: RngCore + CryptoRng>(mut rng: R) -> [u64; 4] {
     bytes
 }
 
+enum Inner {
+    SpendAuth {
+        vk_bytes: VerificationKeyBytes<SpendAuth>,
+        sig: Signature<SpendAuth>,
+        c: Scalar,
+    },
+    Binding {
+        vk_bytes: VerificationKeyBytes<Binding>,
+        sig: Signature<Binding>,
+        c: Scalar,
+    },
+}
+
+impl
+    From<(
+        VerificationKeyBytes<SpendAuth>,
+        Signature<SpendAuth>,
+        Scalar,
+    )> for Inner
+{
+    fn from(
+        tup: (
+            VerificationKeyBytes<SpendAuth>,
+            Signature<SpendAuth>,
+            Scalar,
+        ),
+    ) -> Self {
+        let (vk_bytes, sig, c) = tup;
+        Inner::SpendAuth { vk_bytes, sig, c }
+    }
+}
+
+impl From<(VerificationKeyBytes<Binding>, Signature<Binding>, Scalar)> for Inner {
+    fn from(tup: (VerificationKeyBytes<Binding>, Signature<Binding>, Scalar)) -> Self {
+        let (vk_bytes, sig, c) = tup;
+        Inner::Binding { vk_bytes, sig, c }
+    }
+}
+
 /// A batch verification item.
 ///
 /// This struct exists to allow batch processing to be decoupled from the
 /// lifetime of the message. This is useful when using the batch verification API
 /// in an async context.
-pub struct Item<T: SigType> {
-    vk_bytes: VerificationKeyBytes<T>,
-    sig: Signature<T>,
-    c: Scalar,
+pub struct Item {
+    inner: Inner,
 }
 
-impl<'msg, M: AsRef<[u8]>, T: SigType + ?Sized>
-    From<(VerificationKeyBytes<T>, Signature<T>, &'msg M)> for Item<T>
+impl<'msg, M: AsRef<[u8]>>
+    From<(
+        VerificationKeyBytes<SpendAuth>,
+        Signature<SpendAuth>,
+        &'msg M,
+    )> for Item
 {
-    fn from(tup: (VerificationKeyBytes<T>, Signature<T>, &'msg M)) -> Self {
+    fn from(
+        tup: (
+            VerificationKeyBytes<SpendAuth>,
+            Signature<SpendAuth>,
+            &'msg M,
+        ),
+    ) -> Self {
         let (vk_bytes, sig, msg) = tup;
         // Compute c now to avoid dependency on the msg lifetime.
         let c = HStar::default()
@@ -49,21 +93,45 @@ impl<'msg, M: AsRef<[u8]>, T: SigType + ?Sized>
             .update(&vk_bytes.bytes[..])
             .update(msg)
             .finalize();
-        Self { vk_bytes, sig, c }
+        Self {
+            inner: Inner::SpendAuth { vk_bytes, sig, c },
+        }
     }
 }
 
+impl<'msg, M: AsRef<[u8]>> From<(VerificationKeyBytes<Binding>, Signature<Binding>, &'msg M)>
+    for Item
+{
+    fn from(tup: (VerificationKeyBytes<Binding>, Signature<Binding>, &'msg M)) -> Self {
+        let (vk_bytes, sig, msg) = tup;
+        // Compute c now to avoid dependency on the msg lifetime.
+        let c = HStar::default()
+            .update(&sig.r_bytes[..])
+            .update(&vk_bytes.bytes[..])
+            .update(msg)
+            .finalize();
+        Self {
+            inner: Inner::Binding { vk_bytes, sig, c },
+        }
+    }
+}
+
+// This would ideally be an associated type with `Verifier` but
+// generic associated types are unstable:
+// https://github.com/rust-lang/rust/issues/44265
+type BatchTuple<T: SigType> = (VerificationKeyBytes<T>, Signature<T>, Scalar);
+
 /// A batch verification context.
-pub struct Verifier<T: SigType> {
+pub struct Verifier {
     /// Signature data queued for verification.
-    signatures: Vec<(VerificationKeyBytes<T>, Scalar, Signature<T>)>,
+    signatures: Vec<Item>,
     /// Caching this count avoids a hash traversal to figure out
     /// how much to preallocate.
     batch_size: usize,
 }
 
-impl<T: SigType> Default for Verifier<T> {
-    fn default() -> Verifier<T> {
+impl Default for Verifier {
+    fn default() -> Verifier {
         Verifier {
             signatures: vec![],
             batch_size: usize::default(),
@@ -71,17 +139,15 @@ impl<T: SigType> Default for Verifier<T> {
     }
 }
 
-impl<T: SigType> Verifier<T> {
+impl Verifier {
     /// Construct a new batch verifier.
-    pub fn new() -> Verifier<T> {
+    pub fn new() -> Verifier {
         Verifier::default()
     }
 
-    /// Queue a (key, signature, message) tuple for verification.
-    pub fn queue<I: Into<Item<T>>>(&mut self, item: I) {
-        let Item { vk_bytes, sig, c } = item.into();
-
-        self.signatures.push((vk_bytes, c, sig));
+    /// Queue an Item for verification.
+    pub fn queue<I: Into<Item>>(&mut self, item: I) {
+        self.signatures.push(item.into());
         self.batch_size += 1;
     }
 
@@ -120,59 +186,107 @@ impl<T: SigType> Verifier<T> {
         let mut VKs = Vec::with_capacity(n);
         let mut R_coeffs = Vec::with_capacity(self.batch_size);
         let mut Rs = Vec::with_capacity(self.batch_size);
-        let mut P_coeff = Scalar::zero();
+        let mut P_spendauth_coeff = Scalar::zero();
+        let mut P_binding_coeff = Scalar::zero();
 
-        for (vk_bytes, c, sig) in self.signatures.iter() {
-            let VK = VerificationKey::<T>::try_from(vk_bytes.bytes)
-                .unwrap()
-                .point;
+        for item in self.signatures.iter() {
+            match item.inner {
+                Inner::SpendAuth { vk_bytes, sig, c } => {
+                    // let tup: BatchTuple<SpendAuth> = (vk_bytes, sig, c);
 
-            let mut VK_coeff = Scalar::zero();
+                    // let (P_coeff, R, R_coeff, VK, VK_coeff) =
+                    //     &self.compute_multiscalar_mul_inputs(rng, tup)?;
 
-            let R = {
-                // XXX-jubjub: should not use CtOption here
-                // XXX-jubjub: inconsistent ownership in from_bytes
-                let maybe_point = AffinePoint::from_bytes(sig.r_bytes);
-                if maybe_point.is_some().into() {
-                    jubjub::ExtendedPoint::from(maybe_point.unwrap())
-                } else {
-                    return Err(Error::InvalidSignature);
+                    let z = Scalar::from_raw(gen_128_bits(&mut rng));
+
+                    let s = {
+                        // XXX-jubjub: should not use CtOption here
+                        let maybe_scalar = Scalar::from_bytes(&sig.s_bytes);
+                        if maybe_scalar.is_some().into() {
+                            maybe_scalar.unwrap()
+                        } else {
+                            return Err(Error::InvalidSignature);
+                        }
+                    };
+
+                    let R = {
+                        // XXX-jubjub: should not use CtOption here
+                        // XXX-jubjub: inconsistent ownership in from_bytes
+                        let maybe_point = AffinePoint::from_bytes(sig.r_bytes);
+                        if maybe_point.is_some().into() {
+                            jubjub::ExtendedPoint::from(maybe_point.unwrap())
+                        } else {
+                            return Err(Error::InvalidSignature);
+                        }
+                    };
+                    let P_coeff = z * s;
+                    let R_coeff = z;
+                    let VK = VerificationKey::<SpendAuth>::try_from(vk_bytes.bytes)
+                        .unwrap()
+                        .point;
+                    let VK_coeff = Scalar::zero() + (z * c);
+
+                    P_spendauth_coeff -= P_coeff;
+                    Rs.push(R);
+                    R_coeffs.push(R_coeff);
+                    VKs.push(VK);
+                    VK_coeffs.push(VK_coeff);
+                }
+                Inner::Binding { vk_bytes, sig, c } => {
+                    // let tup: BatchTuple<Binding> = (vk_bytes, sig, c);
+
+                    // let (P_coeff, R, R_coeff, VK, VK_coeff) =
+                    //     &self.compute_multiscalar_mul_inputs(rng, tup)?;
+
+                    let z = Scalar::from_raw(gen_128_bits(&mut rng));
+
+                    let s = {
+                        // XXX-jubjub: should not use CtOption here
+                        let maybe_scalar = Scalar::from_bytes(&sig.s_bytes);
+                        if maybe_scalar.is_some().into() {
+                            maybe_scalar.unwrap()
+                        } else {
+                            return Err(Error::InvalidSignature);
+                        }
+                    };
+
+                    let R = {
+                        // XXX-jubjub: should not use CtOption here
+                        // XXX-jubjub: inconsistent ownership in from_bytes
+                        let maybe_point = AffinePoint::from_bytes(sig.r_bytes);
+                        if maybe_point.is_some().into() {
+                            jubjub::ExtendedPoint::from(maybe_point.unwrap())
+                        } else {
+                            return Err(Error::InvalidSignature);
+                        }
+                    };
+                    let P_coeff = z * s;
+                    let R_coeff = z;
+                    let VK = VerificationKey::<SpendAuth>::try_from(vk_bytes.bytes)
+                        .unwrap()
+                        .point;
+                    let VK_coeff = Scalar::zero() + (z * c);
+
+                    P_binding_coeff -= P_coeff;
+                    Rs.push(R);
+                    R_coeffs.push(R_coeff);
+                    VKs.push(VK);
+                    VK_coeffs.push(VK_coeff);
                 }
             };
-
-            let s = {
-                // XXX-jubjub: should not use CtOption here
-                let maybe_scalar = Scalar::from_bytes(&sig.s_bytes);
-                if maybe_scalar.is_some().into() {
-                    maybe_scalar.unwrap()
-                } else {
-                    return Err(Error::InvalidSignature);
-                }
-            };
-
-            let z = Scalar::from_raw(gen_128_bits(&mut rng));
-
-            P_coeff -= z * s;
-
-            Rs.push(R);
-            R_coeffs.push(z);
-
-            VK_coeff += z * c;
-
-            VKs.push(VK);
-            VK_coeffs.push(VK_coeff);
         }
 
         use std::iter::once;
 
-        let P = &T::basepoint();
+        let scalars = once(&P_spendauth_coeff)
+            .chain(once(&P_binding_coeff))
+            .chain(VK_coeffs.iter())
+            .chain(R_coeffs.iter());
 
-        let check = ExtendedPoint::vartime_multiscalar_mul(
-            once(&P_coeff)
-                .chain(VK_coeffs.iter())
-                .chain(R_coeffs.iter()),
-            once(P).chain(VKs.iter()).chain(Rs.iter()),
-        );
+        let basepoints = [SpendAuth::basepoint(), Binding::basepoint()];
+        let points = basepoints.iter().chain(VKs.iter()).chain(Rs.iter());
+
+        let check = ExtendedPoint::vartime_multiscalar_mul(scalars, points);
 
         if check.is_small_order().into() {
             Ok(())
