2cda7672a3c0f1fe3db10263695695e964e349aa
Deirdre Connolly <deirdre@zfnd.org>
2020-07-03 16:56:01 -0400

    Update benches/bench.rs
    Co-authored-by: Jane Lusby <jlusby42@gmail.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -16,7 +16,7 @@ fn sigs_with_distinct_keys<T: SigType>(
 
 fn bench_batch_verify(c: &mut Criterion) {
     let mut group = c.benchmark_group("Batch Verification");
-    for n in [8usize, 16, 24, 32, 40, 48, 56, 64].iter() {
+    for &n in [8usize, 16, 24, 32, 40, 48, 56, 64].iter() {
         group.throughput(Throughput::Elements(*n as u64));
 
         let sigs = sigs_with_distinct_keys().take(*n).collect::<Vec<_>>();
