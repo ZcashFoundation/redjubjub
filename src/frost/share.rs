90dce62bcfb35bf9f86e15e3bef1f4f08ba927b9
Deirdre Connolly <deirdre@zfnd.org>
2020-10-01 18:30:42 -0400

    Dedupe deps, rename PublicKey for VerificationKey

--- src/frost/share.rs
+++ src/frost/share.rs
@@ -1,4 +1,4 @@
-use crate::{PublicKey, SpendAuth};
+use crate::{SpendAuth, VerificationKey};
 
 /// The threshold analogue of a [`SecretKey`](crate::SecretKey), used for
 /// threshold signing.
@@ -6,8 +6,8 @@ pub struct SecretShare {
     _config: super::Config,
 }
 
-impl<'a> From<&'a SecretShare> for PublicKey<SpendAuth> {
-    fn from(_ss: &'a SecretShare) -> PublicKey<SpendAuth> {
+impl<'a> From<&'a SecretShare> for VerificationKey<SpendAuth> {
+    fn from(_ss: &'a SecretShare) -> VerificationKey<SpendAuth> {
         unimplemented!();
     }
 }
