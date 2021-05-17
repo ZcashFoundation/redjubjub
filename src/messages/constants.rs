65ef751af898babac28e85e1707a5a80bc847ec2
Alfredo Garcia <oxarbitrage@gmail.com>
2021-05-17 12:57:06 -0300

    define and use a new MAX_SIGNERS constant

--- src/messages/constants.rs
+++ src/messages/constants.rs
@@ -16,6 +16,12 @@ pub const AGGREGATOR_PARTICIPANT_ID: u8 = u8::MAX;
 /// We reserve two participant IDs for the dealer and aggregator.
 pub const MAX_SIGNER_PARTICIPANT_ID: u8 = u8::MAX - 2;
 
+/// The maximum number of signers
+///
+/// `MAX_SIGNER_PARTICIPANT_ID` is 253, but the maximum number of signers is actually 254.
+/// (We reserve 2/256 IDs for the dealer and aggregator, leaving 254 valid IDs.)
+pub const MAX_SIGNERS: u8 = MAX_SIGNER_PARTICIPANT_ID + 1;
+
 /// The maximum length of a Zcash message, in bytes.
 pub const ZCASH_MAX_PROTOCOL_MESSAGE_LEN: usize = 2 * 1024 * 1024;
 
